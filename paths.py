b10e4a778095856865c0ff3c8f899ba9f2cb7bcf
chryms0n <qze.agh@gmail.com>
2018-09-22 11:19:14 +0000

    reworked paths.py

--- paths.py
+++ paths.py
@@ -1,12 +1,28 @@
 import sys, os
 
-module_path = os.path.dirname(os.path.abspath(__file__)) + "/../neural-networks-and-deep-learning/src"
-data_path = os.path.dirname(os.path.abspath(__file__)) + "/../neural-networks-and-deep-learning/data"
-### recompile paths in order to cancel out the '/../'
-data_path = os.path.abspath(data_path)
-module_path = os.path.abspath(module_path)
+
+def get_current_path():
+    """
+    Get the path to the current directory.
+    Assumes the file structure /[project]/[Nielson's repo] , [my repo]
+    """
+    return os.path.dirname(os.path.abspath(__file__))
+
+def get_resource_path(dire):
+    """
+    get the path to Michael Nieson's git repository.
+    Assumes the file structure /[project]/[Nielson's repo] , [my repo].
+    """
+    
+    return os.path.abspath(get_current_path() + "/../neural-networks-and-deep-learning/" + dire)
+
+
+module_path = get_resource_path("src")
+data_path = get_resource_path("data")
+current_path = get_current_path()
 
 if __name__ == "__main__":
 
     print(module_path)
     print(data_path)
+    print(current_path)
